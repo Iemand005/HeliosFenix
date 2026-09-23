0f6cd939ca82006f58a789e7b263050252973183
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-09-23 13:45:39 +0200

    Use FELog

--- main.cpp
+++ main.cpp
@@ -1,27 +1,37 @@
-#ifdef _WIN32
+#include "XRGame.hpp"
+#if defined(_WIN32)
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
 #include <windows.h>
-#else
+#elif !defined(__EMSCRIPTEN__) && !defined(__ANDROID__)
 #include <X11/Xlib.h>
 #endif
 #include <string>
-#include <iostream>
 #include "Helios.hpp"
+
+#include <exception>
+
+#include "Log.hpp"
 
 int main() {
 
-	std::cout << "Starting Helios" << std::endl;
+	fe::LogSetTag("Helios");
+	fe::Log("Helios starting");
 
 	try {
+		fe::LogToFile("Creating Helios game instance...");
+
 		Helios game;
+
+		fe::LogToFile("Running game...");
+
 		game.Run();
+
+		fe::LogToFile("Game exited normally");
 	} catch (const std::exception& e) {
-		std::cerr << "Exception caught: " << e.what() << std::endl;
-		return 1;
+		fe::LogToFile(std::string("Exception caught: ") + e.what());
 	} catch (...) {
-		std::cerr << "Unknown exception caught" << std::endl;
-		return 1;
+		fe::LogToFile("Unknown exception caught");
 	}
 
 	return 0;
